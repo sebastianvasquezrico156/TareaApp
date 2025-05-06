fb76723e9c8814d4daa1ce379443d8f6916357b3
jovanny <yovacasta12@gmail>
2025-05-06 07:07:07 -0500

    version secundaria de la lista

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('addTaskBtn').addEventListener('click', function() {
+    const input = document.getElementById('taskInput');
+    const task = input.value.trim();
+    if (task) {
+      const li = document.createElement('li');
+      li.textContent = task;
+      document.getElementById('taskList').appendChild(li);
+      input.value = '';
+    }
+  });
